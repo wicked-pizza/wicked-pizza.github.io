726c3d75f4826ca1f4621d8513b2df3fed9bee58
Hiroshi Hiroshiwa <kesuiket.code@gmail.com>
2021-02-09 00:03:25 +0900

    Add files via upload

--- anagolog-to-pine/assets/js/file.js
+++ anagolog-to-pine/assets/js/file.js
@@ -1,14 +1,35 @@
 
-const file = document.getElementById('file')
+const dropzone = document.getElementById('dropzone')
+const fileinput = document.getElementById('file')
 
-file.addEventListener('change', getFile)
+fileinput.addEventListener('change', function (e) {
+  getFile(e.target.files)
+})
 
-function getFile () {
-  if (!this.files.length > 0) return
+dropzone.addEventListener('dragover', function (e) {
+  e.preventDefault()
+  dropzone.style.background = '#eee'
+})
 
-  const fileData = this.files[0]
+dropzone.addEventListener('dragleave', function (e) {
+  e.preventDefault()
+  dropzone.style.background = '#fff'
+})
+
+dropzone.addEventListener('drop', function (e) {
+  e.preventDefault()
+  dropzone.style.background = '#fff'
+
+  fileinput.files = e.dataTransfer.files
+  getFile(fileinput.files)
+}, false)
+
+function getFile (files) {
+  if (files.length < 1) return
+  if (files.length > 1) return window.alert('アップロードできるファイルは1つだけです')
+
+  const fileData = files[0]
   const reader = new FileReader()
-
 
   reader.onerror = function () {
     alert('ファイルの読み込みに失敗しました')
